880f9a3a35fcda655c21e12559f2fdae265c36bf
klickedicken <klickedicken@live.se>
2017-11-11 18:05:22 +0100

    minor fix part 1, wrote part 2

--- LeapYear1.java
+++ LeapYear1.java
@@ -3,7 +3,7 @@ import static javax.swing.JOptionPane.*;
 public class LeapYear1{
     public static void main (String[] args) {
         String input; 
-        while((input= showInputDialog("Skriv ett årtal")) != null){
+        while((input = showInputDialog("Skriv ett årtal")) != null){
             int year = Integer.parseInt(input);
             
             if(year%4==0 && (year%100!=0 || year%400==0)){
---
+++ LeapYear2.java
@@ -0,0 +1,43 @@
+import static javax.swing.JOptionPane.*;
+
+public class LeapYear2 {
+    public static void main (String[] args) {
+       String input;
+       while((input = showInputDialog("Skriv ett datum på formen åååå-mm-dd")) != null){
+           int day = Integer.parseInt(input.substring(8,10));
+           int month = Integer.parseInt(input.substring(5,7));
+           int year = Integer.parseInt(input.substring(0,4));
+           int n = 0;
+           
+           for(int i=1;month>i;i++) {
+               int m;
+               if(i==1 || i==3 || i==5 || i==7 || i==8 || i==10){
+                   m=31;
+               }
+               else if(i==2){
+                   m=28;
+               }
+               else{
+                   m=30;
+               }
+               n = n + m;
+           } 
+           n = n + day;
+           if(year%4==0 && (year%100!=0 || year%400==0) && month>2){
+               n=n+1;
+           }
+           //if(year%4==0 && (year%100!=0 || year%400==0))
+            showMessageDialog(null,"Dagens nummer är " + n);
+       }
+    }
+}
+
+/* Visa ruta
+gör om string till int, dagar, månader och år för sig med substring
+beräkna dagens nummer... (Hur vet man vilka månader som har olika antal dagar??)
+plus ett om skottår och innan 1 mars
+visa ruta
+
+
+
+*/
