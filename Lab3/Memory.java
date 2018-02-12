7a6a4835925263a88ef4e242ac21369595d65c88
Tobias Rastemo <tobias.rastemo@gmail.com>
2018-02-12 16:22:55 +0100

    i am dog. yes.

---
+++ Lab3/Memory.java
@@ -0,0 +1,6 @@
+public class Memory{
+    public Memory(int n, int m){
+        File folder = new File("mypictures");
+        File[] picture = folder.listFiles();
+    }
+}
--- Lab3/Tools.java
+++ Lab3/Tools.java
@@ -1,8 +1,16 @@
-import.java.util.Random;
+import java.util.Random;
+import java.util.*;
 
 public class Tools{
     public static void randomOrder(Object[] list){
         Random r = new Random();
+        ArrayList<Object> l = new ArrayList<Object>();
+        for (int i = 0; i<list.length;i++){
+            l.add(list[i]);
+        }
+        for (int i=list.length; i>0; i--){
+            list[i-1] = l.remove(r.nextInt(i));
+        }
         
     }
 }
